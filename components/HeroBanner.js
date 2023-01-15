9454f1490d59c71444d32b248f5bb918080e6d30
0xmetamonkey <0xmetamonkey@gmail.com>
2023-01-15 11:57:24 +0530

    first commit

---
+++ components/HeroBanner.js
@@ -0,0 +1,2 @@
+import { css, Global } from '@emotion/core'
+import { useEffect } from 'react'
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className='w-screen h-screen' > <img src='https://cdn.midjourney.com/e8f96288-eceb-45f7-b557-cdf5a5037cb9/grid_0.png'></img></div>
+    
+  )
+}
+
+export default home
