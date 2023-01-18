88980b3f376a8a4b1453c27fdfc40e1009b6cbc4
0xmetamonkey <0xmetamonkey@gmail.com>
2023-01-19 03:14:55 +0530

    first

---
+++ components/home.js
@@ -0,0 +1,20 @@
+import styles from '../styles/home.module.css';
+
+
+
+const Home = () => {
+    return (
+      <div>
+        <Head>
+          <title>My landing page</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <main>
+          <h1>Welcome to my landing page</h1>
+          <p>This is where you can learn more about my app or service.</p>
+        </main>
+      </div>
+    )
+  }
+  
+  
